2b0e10857d2c4bf5f049257fdcffdcab742bb5ea
LinetTorrico <linet.torrico@dharbor.com>
2018-07-06 21:09:43 -0400

    Commit 2 para cherry pick
    Commit 2 para cherry pick

--- src/main/java/com/springwebapp/springwebapp/SpringwebappApplication.java
+++ src/main/java/com/springwebapp/springwebapp/SpringwebappApplication.java
@@ -9,6 +9,6 @@ public class SpringwebappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebappApplication.class, args);
 
-
+		System.out.println("commit 2 para cherry pick");
 	}
 }
